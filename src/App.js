844889abd7a4aebe637705088956b261c9c4b29a
Nick Watson <ngwatson79@yahoo.com>
2020-11-16 17:14:21 -0500

    imported axios to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import axios from "axios";
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(res => {
+    console.log('APOD', res)
+  })
+  .catch(err => console.log('Error!', err))
 
 function App() {
+
+  const [nasaData, setNasaData] = useState([]);
+
+
+
+
+
+
   return (
     <div className="App">
       <p>
