47286726243fdc5829a0d271a9273290d0b317ed
Nick Watson <ngwatson79@yahoo.com>
2020-11-17 22:18:47 -0500

    set up apod file, axios pull

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React, {useState} from "react";
 import "./App.css";
 import axios from "axios";
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  .then(res => {
-    console.log('APOD', res)
-  })
-  .catch(err => console.log('Error!', err))
+
+let date = function() {
+  let today = new Date();
+  let yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1;
+  let dd = today.getDate();
+
+  if (mm < 10) mm = '0' + mm;
+  if (dd < 10) dd = '0' + dd;
+
+  return `${yyyy}-${mm}-${dd}`
+};
+
+console.log(date())
+
+function axiosData() {
+  axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=F5nq55ivbKdgbLmJ82khNLjE0emrxfKho6ddDenJ&date=${date()}`)
+    .then(res => console.log('APOD', res.data))
+    .catch(err => console.log('Something went wrong!!', err));
+}
+
+axiosData()
 
 function App() {
 
